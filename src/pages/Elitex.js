e715234a07a8bf3af0c51ad4d1d82839732474d7
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-10-09 19:16:02 +0200

    elitex section

--- src/pages/Elitex.js
+++ src/pages/Elitex.js
@@ -4,22 +4,62 @@ import { Link } from "gatsby"
 import "./elitex.css"
 import Layout from "../components/layout"
 
+
 class Elitex extends Component {
 
     state = {
-        selector: 1
+        slide: 1
     }
-    // componentDidMount() {
-    //     // var intervalId = setInterval(this.handleArrowRight, 6500);
+    componentDidMount() {
+        var intervalId = setInterval(this.changeSlideRight, 3500);
 
-    //     // this.setState({ intervalId: intervalId });
-    // }
-    // componentWillUnmount() {
+        this.setState({ intervalId: intervalId });
+    }
+    componentWillUnmount() {
 
-    //     clearInterval(this.state.intervalId);
-    // }
+        clearInterval(this.state.intervalId);
+    }
 
+    handleArrow = (e) => {
+        let currentSlide = this.state.slide
+        let direction = e.currentTarget.getAttribute('direction')
+        let nextSlide
+        if (direction === 'right') {
+            nextSlide = currentSlide + 1
+            if (nextSlide > 8) {
+                nextSlide = 1
+            }
+        }
+        if (direction === 'left') {
+            nextSlide = currentSlide - 1
+            if (nextSlide < 1) {
+                nextSlide = 8
+            }
+        }
 
+        this.changeSlide(nextSlide)
+    }
+    changeSlideRight = () => {
+        let currentSlide = this.state.slide
+
+        let nextSlide
+        nextSlide = currentSlide + 1
+        if (nextSlide > 8) {
+            nextSlide = 1
+        }
+        this.changeSlide(nextSlide)
+    }
+
+    changeSlide = (next) => {
+        let allSlides = document.querySelectorAll('.slider__imgs')
+        let nextSlide = document.querySelector(`.slider__img${next}`)
+        allSlides.forEach(function (el) {
+            el.style.display = 'none'
+        })
+        nextSlide.style.display = 'block'
+        this.setState({ slide: next })
+
+    }
 
     render() {
 
@@ -31,16 +71,35 @@ class Elitex extends Component {
         return (
             <Layout>
                 <div className='elitex'>
-
-                    <div className="elitex__content">
-                        <div className="elitex__header">
+                    <div className="elitex__header">
+                        <div className="header__right">
                             <h1 className="new">NOWOŚĆ!</h1>
                             <div className="header__line header__topLine"></div>
-                            <p>Panele Forestia eliteX</p>
-                            <br />
-                            <p>Mocne i solidne panele ścienne do <br /> najbardziej wymagających wnętrz</p>
-                     <h1 className="elitex__title">eliteX</h1>
+                            <div className="header__description">
+                                <p>Panele Forestia eliteX</p>
+                                <br />
+                                <p>Mocne i solidne panele ścienne do <br /> najbardziej wymagających wnętrz</p>
+                            </div>
                         </div>
+                        <h1 className="elitex__title">eliteX</h1>
+                        <div className="header__line header__botLine"></div>
+                    </div>
+                    <div className="elitex__slider">
+                        <div direction='left' onClick={this.handleArrow} className="elitex__arrow elitex__arrowLeft"></div>
+                        <div className="slider__images">
+                            <div className="slider__imgs slider__img1"></div>
+                            <div className="slider__imgs slider__img2"></div>
+                            <div className="slider__imgs slider__img3"></div>
+                            <div className="slider__imgs slider__img4"></div>
+                            <div className="slider__imgs slider__img5"></div>
+                            <div className="slider__imgs slider__img6"></div>
+                            <div className="slider__imgs slider__img7"></div>
+                            <div className="slider__imgs slider__img8"></div>
+                        </div>
+                        <div direction='right' onClick={this.handleArrow} className="elitex__arrow elitex__arrowRight"></div>
+                    </div>
+                    <div className="elitex__content">
+
                         <div className="elitex__description">
                             <p> Forestia eliteX to idealny wybór do garaży, hal przemysłowych, budynków <br />
                                 gospodarczych, magazynów, pralni, rzeźni, mleczarni, <br />
@@ -87,8 +146,24 @@ class Elitex extends Component {
                         </div>
                         <div className="elitex__bars">
                             <div className="bar">
-                                <div className="bar__img"></div>
-                                <div className="bar__text"></div>
+                                <div className="bar__img bar1"></div>
+                                <div className="bar__text">Listwa pozioma</div>
+                            </div>
+                            <div className="bar">
+                                <div className="bar__img bar2"></div>
+                                <div className="bar__text">Listwa narożna zewnętrzna</div>
+                            </div>
+                            <div className="bar">
+                                <div className="bar__img bar3"></div>
+                                <div className="bar__text">Listwa pionowa dwuczęściowa</div>
+                            </div>
+                            <div className="bar">
+                                <div className="bar__img bar4"></div>
+                                <div className="bar__text">Listwa narożna wewnętrzna dwuczęściowa</div>
+                            </div>
+                            <div className="bar">
+                                <div className="bar__img bar5"></div>
+                                <div className="bar__text">Listwa przypodłogowa</div>
                             </div>
 
                         </div>
